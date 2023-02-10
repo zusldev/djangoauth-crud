255cdc893dd0a431d05ac47abf6b214f6f8b7db1
Liborzdev <liborio07zs@gmail.com>
2023-02-10 12:35:47 -0700

    update staticfiles settings

--- djangoCrud/settings.py
+++ djangoCrud/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'tasks/staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
