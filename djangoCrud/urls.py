347f5fdffedb403d7a03571918aa485f8cec60cc
Liborzdev <liborio07zs@gmail.com>
2023-02-10 11:17:14 -0700

    url home

--- djangoCrud/urls.py
+++ djangoCrud/urls.py
@@ -5,7 +5,7 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('tasks/', views.tasks, name='tasks'),
     path('tasks/completelist', views.completelist, name='completeList'),
